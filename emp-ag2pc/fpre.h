6ee35034a7a6fc230b1cebd964c46121c491648a
Joe Rowell <joerowell4@gmail.com>
2022-11-09 12:16:58 +0000

    Remove check on nullptr before delete

--- emp-ag2pc/fpre.h
+++ emp-ag2pc/fpre.h
@@ -103,10 +103,10 @@ class Fpre {
 				permute_batch_size = 3100;
 			}
 			else bucket_size = 5;
-			if (MAC != nullptr) {
-				delete[] MAC;
-				delete[] KEY;
-			}
+			
+			delete[] MAC;
+			delete[] KEY;
+	      
 			MAC = new block[batch_size * bucket_size * 3];
 			KEY = new block[batch_size * bucket_size * 3];
 			MAC_res = new block[batch_size * 3];
@@ -114,12 +114,11 @@ class Fpre {
 //			cout << size<<"\t"<<batch_size<<"\n";
 		}
 		~Fpre() {
-			if(MAC != nullptr) {
-				delete[] MAC;
-				delete[] KEY;
-			}
-			if (MAC_res != nullptr) delete[] MAC_res;
-			if (KEY_res != nullptr) delete[] KEY_res;
+
+			delete[] MAC;
+		        delete[] KEY;
+			delete[] MAC_res;
+			delete[] KEY_res;
 			delete[] prps;
 			delete pool;
 			for(int i = 0; i < THDS; ++i) {
